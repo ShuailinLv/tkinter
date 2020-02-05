887f23cb5ea0e29ee376dfc4c4d6b5628df7e2ed
吕帅林 <lvshuailin@gmail.com>
2020-02-05 17:42:23 +0800

    example 1 revise

--- t1.py
+++ t1.py
@@ -4,8 +4,23 @@ window = tk.Tk()
 window.title('first window')
 window.geometry('200x100')
 
-l = tk.Label(window, text='hello tkinter', bg='green', font=('Arial, 12'), width=15, height=2)
+var = tk.StringVar()
+l = tk.Label(window, textvariable=var, bg='green', font=('Arial, 12'), width=15, height=2)
 l.pack()
+
+on_hit = False
+def hit_me():
+	global on_hit
+	if on_hit == False:
+		on_hit = True
+		var.set('hello tkinter')
+	else:
+		on_hit = False
+		var.set('')
+
+b = tk.Button(window, text='hit me', width = 15, height=2, command=hit_me)
+b.pack()
+
 window.mainloop()
 
 
